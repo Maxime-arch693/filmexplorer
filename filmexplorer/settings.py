359560d46ed07f1a6b7955d3ceb7baa730493eba
maxime <maxime.couton-baudoin@etu.univ-amu.fr>
2026-03-16 14:57:57 +0100

    Fix allowed hosts and dependencies

--- filmexplorer/settings.py
+++ filmexplorer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u0d9r6lj437$qi_ljf4-m7fg+*m7zfeur#tph=c&u89q3f7q*y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['filmexplorer-maxime.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
